b5d38dea04e323e243b80a747ec0094b9829c40e
Abdallah Eldesouky <abdallaheldesoky301@gmail.com>
2025-04-27 17:33:42 +0300

    createDatabaseEntities

---
+++ BlazorInvoiceApp/Data/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorInvoiceApp.Data
+{
+    public class Invoice : IEntity, IOwnedEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string UserId { get; set; } = null!;
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int InvoiceNumber { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public string Description { get; set; } = String.Empty;
+        public string CustomerId { get; set; } = String.Empty;
+        public string InvoiceTermsId { get; set; } = String.Empty;
+    }
+}
