ca2974a174a1d025e78b54d304bc4163106fff6f
Abdallah Eldesouky <abdallaheldesoky301@gmail.com>
2025-04-27 17:58:53 +0300

    navigationalProberties

--- BlazorInvoiceApp/Data/Customer.cs
+++ BlazorInvoiceApp/Data/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorInvoiceApp.Data
 {
@@ -7,6 +8,7 @@ namespace BlazorInvoiceApp.Data
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string UserId { get; set; } = null!;
+        public IdentityUser? User { get; set; } = null!;
         public string Name { get; set; } = String.Empty;
     }
 }
